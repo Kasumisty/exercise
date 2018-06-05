41b5e749d9c13fe0be3a59889fc196b4ee35482b
Kasumisty Liu <kasumisty@qq.com>
2018-06-05 22:54:50 +0800

    create_file

--- 2.10.cpp
+++ 2.10.cpp
@@ -1,3 +1,4 @@
+// 查找学生信息
 #include <stdio.h>
 #include <string.h>
 #include <algorithm>
@@ -21,36 +22,38 @@ int main(int argc, char const *argv[])
 			scanf("%s%s%s%d", buf[i].no, buf[i].name, buf[i].sex, &buf[i].age);
 		}
 		sort(buf, buf+n);
-		for(int i=0; i<n; i++){
-			printf("%s\n", buf[i].no);
-		}
-
-		// int t;
-		// scanf("%d", &t);
-		// while(t-- != 0){
-		// 	char search[20];
-		// 	scanf("%s", search);
-		// 	int ans=-1, base=0, top=n-1;
-		// 	int mid;
-		// 	while(base<top){
-		// 		mid = (base+top)/2;
-		// 		int tmp = strcmp(search, buf[mid].no);
-		// 		if(tmp == 0)
-		// 			ans = mid;
-		// 		else if(tmp < 0){
-		// 			top = mid-1;
-		// 		}
-		// 		else{
-		// 			base = mid+1;
-		// 		}
-		// 	}
-		// 	if(ans == -1){
-		// 		printf("%s", "No Answer!");
-		// 	}
-		// 	else{
-		// 		printf("%s %s %s %d\n", buf[ans].no, buf[ans].name, buf[ans].sex, buf[ans].age);
-		// 	}
+		// for(int i=0; i<n; i++){
+		// 	printf("%s\n", buf[i].no);
 		// }
+
+		int t;
+		scanf("%d", &t);
+		while(t-- != 0){
+			char search[20];
+			scanf("%s", search);
+			int ans=-1, base=0, top=n-1;
+			int mid;
+			while(base<=top){
+				mid = (base+top)/2;
+				int tmp = strcmp(search, buf[mid].no);
+				if(tmp == 0){
+					ans = mid;
+					break;
+				}
+				else if(tmp < 0){
+					top = mid-1;
+				}
+				else{
+					base = mid+1;
+				}
+			}
+			if(ans == -1){
+				printf("%s", "No Answer!");
+			}
+			else{
+				printf("%s %s %s %d\n", buf[ans].no, buf[ans].name, buf[ans].sex, buf[ans].age);
+			}
+		}
 	}
 	return 0;
 }
